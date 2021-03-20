0dbe5f615c6229be17c3ad836734cf2cec38e407
Simeon Cheeseman <simeon@tablecheck.com>
2021-03-20 13:06:42 +0900

    Update CHANGELOG.md [skip ci]

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# v0.0.1 (Sat Mar 20 2021)
+
+#### ⚠️ Pushed to `main`
+
+- Initial commit ([@SimeonC](https://github.com/SimeonC))
+
+#### Authors: 1
+
+- Simeon Cheeseman ([@SimeonC](https://github.com/SimeonC))
---
+++ src/preset/constants.js
@@ -0,0 +1,10 @@
+export const ADDON_ID = "storybook/xstate";
+export const PANEL_ID = `${ADDON_ID}/panel`;
+export const PARAM_KEY = `xstate`;
+export const INSPECT_ID = "xstate-storybook-addon";
+
+export const EVENTS = {
+  RESULT: `${ADDON_ID}/result`,
+  REQUEST: `${ADDON_ID}/request`,
+  CLEAR: `${ADDON_ID}/clear`,
+};
