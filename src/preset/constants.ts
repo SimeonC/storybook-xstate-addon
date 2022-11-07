239b86a405327e381945e8d4e22a1567f17a3997
Hakim Ch <hchmimo@lacentrale.fr>
2022-11-07 19:09:00 +0100

    chore: remove unused constant

--- src/preset/constants.ts
+++ src/preset/constants.ts
@@ -1,6 +1,5 @@
 export const ADDON_ID = "storybook/xstate";
 export const PANEL_ID = `${ADDON_ID}/panel`;
-export const PARAM_KEY = `xstate`;
 export const INSPECT_ID = "xstate-storybook-addon";
 
 export const EVENTS = {
